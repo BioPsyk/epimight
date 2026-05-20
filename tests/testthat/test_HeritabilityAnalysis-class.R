76b909f0bb18f9c9d658feea6fe0cbf64998b247
Richard Zetterberg <richard.zetterberg@gmail.com>
2026-05-20 14:38:15 +0200

    Removes legacy tests

--- tests/testthat/test_HeritabilityAnalysis-class.R
+++ tests/testthat/test_HeritabilityAnalysis-class.R
@@ -2,7 +2,6 @@ library(testthat, quietly = TRUE, warn.conflicts = FALSE)
 library(data.table, quietly = TRUE, warn.conflicts = FALSE)
 library(parallel, quietly = TRUE, warn.conflicts = FALSE)
 
-source("../legacy.R")
 source("../utils.R")
 
 #=================================================================================
@@ -173,85 +172,5 @@ describe("run", {
     )
 
     validator$run(meta = h2_analysis$run_meta(results))
-  })
-
-  it("Produces same results as dk_h2byYOB", {
-    #=================================================================================
-    # Old results
-    #=================================================================================
-
-    old_results <- dk_h2byYOB("MDD", tte, 1, 100, "a1", "PO", "fixed") |>
-      as.data.table() |>
-      select(
-        Year, h2, Variance, L95, U95
-      ) |>
-      rename(
-        born_at_year = Year,
-        se           = Variance,
-        l95          = L95,
-        u95          = U95
-      ) |>
-      filter_all(
-        all_vars(!is.infinite(.) & !is.na(.))
-      )
-
-    #=================================================================================
-    # New results
-    #=================================================================================
-
-    tte2 <- tte_dt |>
-      mutate(
-        born_at_year = as.character(format(born_at, "%Y"))
-      )
-
-    tte2 <- tte2 |>
-      mutate(cohort = "all") |>
-      union_all(
-        tte2 |>
-        filter(diagnosed_relatives > 0) |>
-        mutate(cohort = "affected_relatives")
-      )
-
-    estimates <- cif_analysis$run(
-      tte = tte2,
-      group_columns = list("born_at_year", "cohort")
-    )
-
-    cohort1 <- estimates |> filter(cohort == "all")
-    cohort2 <- estimates |> filter(cohort == "affected_relatives")
-
-    combined <- cohort1 |>
-      inner_join(cohort2, by = join_by(time, born_at_year)) |>
-      rename(
-        c1_estimate = estimate.x,
-        c1_cases    = cases.x,
-        c2_estimate = estimate.y,
-        c2_cases    = cases.y
-      ) |>
-      select(time, born_at_year, c1_estimate, c1_cases, c2_estimate, c2_cases) |>
-      group_by(born_at_year) |>
-      arrange(desc(time)) |>
-      filter(row_number() == 1) |>
-      as.data.table()
-
-    new_results <- h2_analysis$run(
-      relationship_kind = relationship_kind,
-      estimates         = combined
-    ) |> select(born_at_year, h2, se, l95, u95)
-
-    new_meta <- h2_analysis$run_meta(new_results) |>
-      select(starts_with("fixed_")) |>
-      mutate(born_at_year = "Meta_fixed") |>
-      rename(
-        h2  = fixed_meta,
-        se  = fixed_se,
-        l95 = fixed_l95,
-        u95 = fixed_u95
-      )
-
-    new_results <- new_results |> rbind(new_meta) |> arrange(desc(born_at_year))
-    old_results <- old_results |> arrange(desc(born_at_year))
-
-    expect_dataframe_equal(old_results, new_results)
   })
 })
