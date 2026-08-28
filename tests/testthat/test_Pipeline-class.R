340b1402b783333f24e15455edddccbcb1e522df
Richard Zetterberg <richard.zetterberg@gmail.com>
2026-08-28 11:47:08 +0200

    Adds missing tests

--- tests/testthat/test_Pipeline-class.R
+++ tests/testthat/test_Pipeline-class.R
@@ -20,71 +20,71 @@ pipeline <- Pipeline$new(pool = pool_tte)
 # Tests
 #=================================================================================
 
-#describe("initialize", {
-#  it("doesn't allow empty arguments", {
-#    expect_error(Pipeline$new())
-#  })
-#
-#  it("doesn't allow wrong tte type", {
-#    expect_error(Pipeline$new(
-#      pool = "hello"
-#    ))
-#
-#    expect_error(Pipeline$new(
-#      pool = 21
-#    ))
-#
-#    expect_error(Pipeline$new(
-#      pool = FALSE
-#    ))
-#  })
-#
-#  it("doesn't allow wrong typed relative kinds", {
-#    expect_error(Pipeline$new(
-#      pool = data.table(
-#        person_id         = c("p1", "p1"),
-#        birth_year        = c(1950, 1951),
-#        trait             = c("SCZ", "CAD"),
-#        trait_status      = c(0, 1),
-#        trait_age        = c(10, 10),
-#        relatives_kind    = c(10, 20), # Wrong type
-#        relatives_n       = c(1, 1),
-#        relatives_n_trait = c(0, 0)
-#      )
-#    ))
-#  })
-#
-#  it("doesn't allow numeric person IDs", {
-#    expect_error(Pipeline$new(
-#      pool = data.table(
-#        person_id         = c(1, 1),
-#        birth_year        = c(1950, 1951),
-#        trait             = c("SCZ", "CAD"),
-#        trait_status      = c(0, 1),
-#        trait_age       = c(10, 20),
-#        relatives_kind    = c("parents", "parents"),
-#        relatives_n       = c(2, 2),
-#        relatives_n_trait = c(0, 1)
-#      )
-#    ))
-#  })
-#
-#  it("allows valid tte", {
-#    Pipeline$new(
-#      pool = data.table(
-#        person_id         = c("p1", "p1"),
-#        birth_year        = c(1950, 1951),
-#        trait             = c("SCZ", "CAD"),
-#        trait_status      = c(0, 1),
-#        trait_age       = c(10, 20),
-#        relatives_kind    = c("parents", "parents"),
-#        relatives_n       = c(2, 2),
-#        relatives_n_trait = c(0, 1)
-#      )
-#    )
-#  })
-#})
-#
+describe("initialize", {
+  it("doesn't allow empty arguments", {
+    expect_error(Pipeline$new())
+  })
+
+  it("doesn't allow wrong tte type", {
+    expect_error(Pipeline$new(
+      pool = "hello"
+    ))
+
+    expect_error(Pipeline$new(
+      pool = 21
+    ))
+
+    expect_error(Pipeline$new(
+      pool = FALSE
+    ))
+  })
+
+  it("doesn't allow wrong typed relative kinds", {
+    expect_error(Pipeline$new(
+      pool = data.table(
+        person_id         = c("p1", "p1"),
+        birth_year        = c(1950, 1951),
+        trait             = c("SCZ", "CAD"),
+        trait_status      = c(0, 1),
+        trait_age        = c(10, 10),
+        relatives_kind    = c(10, 20), # Wrong type
+        relatives_n       = c(1, 1),
+        relatives_n_trait = c(0, 0)
+      )
+    ))
+  })
+
+  it("doesn't allow numeric person IDs", {
+    expect_error(Pipeline$new(
+      pool = data.table(
+        person_id         = c(1, 1),
+        birth_year        = c(1950, 1951),
+        trait             = c("SCZ", "CAD"),
+        trait_status      = c(0, 1),
+        trait_age       = c(10, 20),
+        relatives_kind    = c("parents", "parents"),
+        relatives_n       = c(2, 2),
+        relatives_n_trait = c(0, 1)
+      )
+    ))
+  })
+
+  it("allows valid tte", {
+    Pipeline$new(
+      pool = data.table(
+        person_id         = c("p1", "p1"),
+        birth_year        = c(1950, 1951),
+        trait             = c("SCZ", "CAD"),
+        trait_status      = c(0, 1),
+        trait_age       = c(10, 20),
+        relatives_kind    = c("parents", "parents"),
+        relatives_n       = c(2, 2),
+        relatives_n_trait = c(0, 1)
+      )
+    )
+  })
+})
+
 describe("get_tte", {
   it("fails on unknown stratify_columns", {
     expect_error(pipeline$get_tte(
@@ -161,112 +161,112 @@ describe("get_tte", {
 })
 
 describe("run", {
-  #it("doesn't allow empty arguments", {
-  #  expect_error(pipeline$run())
-  #})
+  it("doesn't allow empty arguments", {
+    expect_error(pipeline$run())
+  })
 
-  #it("fails when traits are not found", {
-  #  expect_error(pipeline$run(
-  #    heritability1 = list(
-  #      trait          = "unknown",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    ),
-  #    heritability2 = list(
-  #      trait          = "CAD",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    ),
-  #    genetic_correlation = list(
-  #      trait          = "CAD",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    )
-  #  ))
+  it("fails when traits are not found", {
+    expect_error(pipeline$run(
+      heritability1 = list(
+        trait          = "unknown",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      heritability2 = list(
+        trait          = "CAD",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      genetic_correlation = list(
+        trait          = "CAD",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      )
+    ))
 
-  #  expect_error(pipeline$run(
-  #    heritability1 = list(
-  #      trait          = "SCZ",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    ),
-  #    heritability2 = list(
-  #      trait          = "unknown",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    ),
-  #    genetic_correlation = list(
-  #      trait          = "CAD",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    )
-  #  ))
-  #})
+    expect_error(pipeline$run(
+      heritability1 = list(
+        trait          = "SCZ",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      heritability2 = list(
+        trait          = "unknown",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      genetic_correlation = list(
+        trait          = "CAD",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      )
+    ))
+  })
 
-  #it("fails when stratify column cannot be found in TTE dataset", {
-  #  expect_error(pipeline$run(
-  #    heritability1 = list(
-  #      trait          = "SCZ",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    ),
-  #    heritability2 = list(
-  #      trait          = "CAD",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    ),
-  #    genetic_correlation = list(
-  #      trait          = "CAD",
-  #      relatives_kind = "parents",
-  #      relatedness    = 0.5
-  #    ),
-  #    stratify_columns = list("birth_year", "unknown")
-  #  ))
-  #})
-#
-#  it("produces different result when using weighted cif", {
-#    results <- pipeline$run(
-#      heritability1 = list(
-#        index_trait    = "SCZ",
-#        relatives_kind = "parents",
-#        relatedness    = 0.5
-#      ),
-#      heritability2 = list(
-#        index_trait    = "CAD",
-#        relatives_kind = "parents",
-#        relatedness    = 0.5
-#      ),
-#      genetic_correlation = list(
-#        index_trait     = "CAD",
-#        relatives_trait = "CAD",
-#        relatives_kind  = "parents",
-#        relatedness     = 0.5
-#      ),
-#      stratify_columns = list("birth_year"),
-#      use_weighted_cif = FALSE
-#    )
-#
-#    weighted_results <- pipeline$run(
-#      heritability1 = list(
-#        index_trait    = "SCZ",
-#        relatives_kind = "parents",
-#        relatedness    = 0.5
-#      ),
-#      heritability2 = list(
-#        index_trait    = "CAD",
-#        relatives_kind = "parents",
-#        relatedness    = 0.5
-#      ),
-#      genetic_correlation = list(
-#        index_trait     = "SCZ",
-#        relatives_trait = "CAD",
-#        relatives_kind  = "parents",
-#        relatedness     = 0.5
-#      ),
-#      stratify_columns = list("birth_year"),
-#      use_weighted_cif = TRUE
-#    )
-#
-#    expect_dataframe_not_equal(results$rg, weighted_results$rg)
-#  })
+  it("fails when stratify column cannot be found in TTE dataset", {
+    expect_error(pipeline$run(
+      heritability1 = list(
+        trait          = "SCZ",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      heritability2 = list(
+        trait          = "CAD",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      genetic_correlation = list(
+        trait          = "CAD",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      stratify_columns = list("birth_year", "unknown")
+    ))
+  })
+
+  it("produces different result when using weighted cif", {
+    results <- pipeline$run(
+      heritability1 = list(
+        index_trait    = "SCZ",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      heritability2 = list(
+        index_trait    = "CAD",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      genetic_correlation = list(
+        index_trait     = "CAD",
+        relatives_trait = "CAD",
+        relatives_kind  = "parents",
+        relatedness     = 0.5
+      ),
+      stratify_columns = list("birth_year"),
+      use_weighted_cif = FALSE
+    )
+
+    weighted_results <- pipeline$run(
+      heritability1 = list(
+        index_trait    = "SCZ",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      heritability2 = list(
+        index_trait    = "CAD",
+        relatives_kind = "parents",
+        relatedness    = 0.5
+      ),
+      genetic_correlation = list(
+        index_trait     = "SCZ",
+        relatives_trait = "CAD",
+        relatives_kind  = "parents",
+        relatedness     = 0.5
+      ),
+      stratify_columns = list("birth_year"),
+      use_weighted_cif = TRUE
+    )
+
+    expect_dataframe_not_equal(results$rg, weighted_results$rg)
+  })
 })
