19b038dca25127350dce88c498d921935d8a5b67
Richard Zetterberg <richard.zetterberg@gmail.com>
2026-09-10 09:25:29 +0200

    Removes redundant command

--- scripts/build-container-image.sh
+++ scripts/build-container-image.sh
@@ -7,8 +7,5 @@ project_dir=$(dirname "$script_dir")
 
 cd "${project_dir}"
 
-echo ">> Building singularity image"
-nix build .#singularityImage
-
 echo ">> Building docker image"
 nix build .#dockerImage
