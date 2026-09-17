1613986d34721e90d99916d279ad8cfae38761b2
Richard Zetterberg <richard.zetterberg@gmail.com>
2026-09-17 09:13:36 +0200

    Fixes missing extdata in containers

--- src.nix
+++ src.nix
@@ -9,13 +9,14 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out
 
-    cp ${./R} $out/R -R
-    cp ${./guides} $out/guides -R
-    cp ${./tests} $out/tests -R
     cp ${./DESCRIPTION} $out/DESCRIPTION -R
     cp ${./NAMESPACE} $out/NAMESPACE -R
     cp ${./NEWS.md} $out/NEWS.md -R
     cp ${./README.md} $out/README.md
+    cp ${./R} $out/R -R
     cp ${./VERSION} $out/VERSION -R
+    cp ${./guides} $out/guides -R
+    cp ${./inst} $out/inst -R
+    cp ${./tests} $out/tests -R
   '';
 }
